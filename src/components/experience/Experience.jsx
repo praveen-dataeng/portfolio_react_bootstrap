4a2c88d1aace62357664b748759d254ff5d095ac
Praveen Kumar <praveen.elesup@outlook.com>
2022-07-27 16:31:34 +0530

    Experience section complited

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,33 +1,71 @@
 import React from 'react';
 import './experience.css';
-// Bootstrap imports
-import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
+import IBM from './../../assets/images/IBM_logo.png';
+import Cryptograph from './../../assets/images/cgt.png';
+import ValueC from './../../assets/images/valueC.png';
+
+// Bootstrap Imports
+import Button from 'react-bootstrap/Button';
+
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+// Companies details as Array
+const data = [
+  {
+    logo : IBM,
+    name : "IBM India Pvt., Ltd.,",
+    experience : "Worked as Mainframe Engineer"
+  },
+  {
+    logo : Cryptograph,
+    name : "Cryptograph India",
+    experience : "Worked as ETL Engineer"
+  },
+  {
+    logo  : ValueC,
+    name  : "Value C consulting Services Pvt., Ltd.,",
+    experience  : "Woking as Senior Web Developer",
+  }
+]
 
 const Experience = () => {
   return (
-    <>
-    <section id="experience">
-      <h1>Experience</h1>
-      <CardGroup className='text-center'>
-            <Card>              
-              <Card.Body>
-                <Card.Title>Web Development</Card.Title>
-                <Card.Text>
-                  <ul>
-                    <li>Option 1</li>
-                    <li>Option 1</li>
-                    <li>Option 1</li>
-                    <li>Option 1</li>
-                    <li>Option 1</li>
-                  </ul>                  
-                </Card.Text>
-              </Card.Body>            
-            </Card>
-    </CardGroup>
+    <section id='experience'>
+      <h5 className='text-center text-white'>Companie worked</h5>    
+      <h1 className='text-center text-white'>Experience</h1>
+      <hr />
+      
+      <Swiper className="container experience__container"
+        // install Swiper modules
+      modules={[Pagination ]}
+      spaceBetween={40}
+      slidesPerView={1}      
+      pagination={{ clickable: true }}
+      >
+        {
+          data.map(({logo, name, experience}, index) => {
+            return (
+              <SwiperSlide key={index} className='companies'>
+                <div className="company__logo">
+                  <img className='fit-picture' src={logo} alt="Image+'${logo}'"/>
+                </div>
+                <h2>{name}</h2>
+                <small>{experience}</small>
+              </SwiperSlide>
+            )
+          })
+        }
+      </Swiper>
     </section>
-    </>
-  );
+  )
 }
 
 export default Experience
--- src/index.js
+++ src/index.js
@@ -6,9 +6,6 @@ import React from "react";
 import { createRoot } from 'react-dom/client';
 import './index.css';
 
-// Bootstrap stylesheet
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 import App from './App.jsx';
 
 const container = document.getElementById('root');
